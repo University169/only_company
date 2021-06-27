d565053e0a6ff0a524c796056209944cd7b836c6
University169 <dmklimkov@gmail.com>
2021-06-27 17:07:27 +0200

    add django-debug-toolbar

--- src/only_company/urls.py
+++ src/only_company/urls.py
@@ -14,11 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
 
 from city_person import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.PersonsView.as_view())
+    path('', views.PersonsView.as_view()),
 ]
+
+if settings.DEBUG:
+    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
+
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
+    import mimetypes
+
+    mimetypes.add_type("application/javascript", ".js", True)
