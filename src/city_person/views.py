bdeff3885a93fb62c0301cdfadb712a3c31fff98
University169 <dmklimkov@gmail.com>
2021-06-27 17:35:47 +0200

    optimized queries

--- src/city_person/views.py
+++ src/city_person/views.py
@@ -4,5 +4,5 @@ from .models import City, Person
 
 
 def show_persons(request):
-    persons = Person.objects.all()
+    persons = Person.objects.select_related('city')
     return render(request, "city_person/home.html", {'persons': persons})
